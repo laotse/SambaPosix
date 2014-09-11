ca2027f3cf79c9502d8a587c7cfe24bea152cd64
Dr. Lars Hanke <lars@tan-tra.de>
2014-09-11 22:14:11 +0200

    Added group set and group add
    * sanitized options

--- SambaPosix/Group.py
+++ SambaPosix/Group.py
@@ -7,6 +7,8 @@ Created on 11.09.2014
 from SambaPosix.Command import Command
 from optparse import OptionGroup
 
+import ldap
+
 class Group(Command):
     '''
     classdocs
@@ -34,6 +36,11 @@ class Group(Command):
         group.add_option("-g", "--gid", dest="gid", help="set numerical group ID", metavar="GID")
         parser.add_option_group(group)
         return parser
+
+    def sanitizeOptions(self):
+        if not self.checkPOSIXID(self.opts.gid):
+            raise ValueError("%s is not valid for group id" % self.opts.gid)
+        return Command.sanitizeOptions(self)
 
     def resolveMembers(self,entry, recursive = True):
         members = []
@@ -71,16 +78,51 @@ class Group(Command):
         for entry in accounts:
             self.result(self.formatAsGetent(entry))
 
+    def locateByGID(self,gid):
+        entries = self.search('(&(objectClass=posixGroup)(gidNumber=%s))' % self.opts.gid)
+        accounts = []
+        for record in entries:
+            if 'sAMAccountName' in record:
+                accounts += record['sAMAccountName']
+        return accounts
+
+    def locateFirstGroup(self):
+        group = None
+        if len(self.args) > 0:
+            group = self.args[0]
+            if not self.checkPosixName(group):
+                raise ValueError("Group name %s invalid" % group)
+            entries = self.search('(&(sAMAccountName=%s)(objectClass=group))' % group)
+            if len(entries) > 1:
+                self.result("Error: %d matching groups for %s" % (len(entries),group))
+            if len(entries) == 1:
+                self.args = self.args[1:]
+                return group,entries[0]
+
+        if self.opts.gid is not None:
+            entries = self.search('(&(objectClass=posixGroup)(gidNumber=%s)(objectClass=group))' % self.opts.gid)
+            if len(entries) > 1:
+                self.result("Error: %d matching groups for %s" % (len(entries),self.opts.gid))
+            if len(entries) == 1:
+                return entries[0].getSingleValue('sAMAccountName'), entries[0]
+
+        return None, None
+
+    def locateUser(self, name):
+        entries = self.search('(&(sAMAccountName=%s)(objectClass=user))' % name)
+        if len(entries) > 1:
+            self.error("Error: %d matching users for %s" % (len(entries),name))
+            return None
+        if len(entries) < 1:
+            return None
+        return entries[0].dn()
+
     def do_show(self):
         if len(self.args) > 0:
             accounts = self.args
         else: accounts = None
         if accounts is None and self.opts.gid is not None:
-            entries = self.search('(&(objectClass=posixGroup)(gidNumber=%s))' % self.opts.gid)
-            accounts = []
-            for record in entries:
-                if 'sAMAccountName' in record:
-                    accounts += record['sAMAccountName']
+            accounts = self.locateByGID(self.opts.gid)
         if accounts is None:
             self.error("No group specified to show")
             return
@@ -98,3 +140,45 @@ class Group(Command):
                     self.result(self.formatAsGetent(entry))
                     if 'userPassword' in entry or 'unixUserPassword' in entry:
                         self.result("#%s has unix password entries set!" % group)
+
+    def do_set(self):
+        group, entry = self.locateFirstGroup()
+        if group is None:
+            self.error("No group specified for modification")
+            return
+
+        modify = []
+        if not entry.hasAttribute('objectClass', 'posixGroup'):
+            modify += [(ldap.MOD_ADD, 'objectClass', 'posixGroup')]
+        gid = entry.getSingleValue('gidNumber')
+        if gid is None:
+            modify += [(ldap.MOD_ADD, 'gidNumber', self.opts.gid)]
+        elif gid != self.opts.gid:
+            modify += [(ldap.MOD_REPLACE, 'gidNumber', self.opts.gid)]
+
+        if len(modify) > 0:
+            self.modify(entry.dn(), modify)
+
+    def do_add(self):
+        group, entry = self.locateFirstGroup()
+        if group is None:
+            self.error("No group specified for adding users")
+            return
+
+        modify = []
+        for user in self.args:
+            if not self.checkPosixName(user):
+                self.error("User name %s invalid -- skipped" % user)
+                continue
+            dn = self.locateUser(user)
+            if dn is None:
+                self.error("Unknown user %s -- skipped" % user)
+            else:
+                if not entry.hasAttribute('member', dn):
+                    modify += [(ldap.MOD_ADD, 'member', dn)]
+
+            if len(modify) > 0:
+                # FIXME: raises ldap.ALREADY_EXISTS on the second user - adding single users works fine
+                self.modify(entry.dn(), modify)
+
+
